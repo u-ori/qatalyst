6ae139d1a482acc438cfb45d9ff478f42b24d606
Ori <77636823+oridev-net@users.noreply.github.com>
2023-09-26 20:55:40 +0000

    inint

---
+++ src/routes/+page.svelte
@@ -0,0 +1,11 @@
+<div>
+    <h1>Quanta</h1>
+    <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+</div>
+
+<style>
+    h1 {
+        font-family: 'Rubik', sans-serif;
+        font-size: 5rem;
+    }
+</style>
