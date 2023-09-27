ffb18e0951c80f1fd9b5da4f9fb744917f52830c
Ori <dibobykin@gmail.com>
2023-09-26 21:45:42 -0400

    fgcvxc

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,11 +1,42 @@
+<script>
+    let recentSeas = ["C:/Sea"]
+</script>
+
 <div>
     <h1>Quanta</h1>
-    <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+    {#each recentSeas as location}
+        <a href="">{location}</a>
+    {/each}
+    <button>Make a new Sea</button>
+    <button>Open a Sea</button>
 </div>
 
 <style>
+    div {
+        display: flex;
+        flex-direction: column;
+        background-color: #c4a1ff;
+        border-color: #000;
+        border-width: 2px;
+        border-style: solid;
+        border-radius: 1rem;
+        padding: 1rem;
+        position: absolute;
+        top: 50%;
+        left: 50%;
+        transform: translate(-50%, -50%);
+        box-shadow: 4px 6px;
+    }
     h1 {
         font-family: 'Rubik', sans-serif;
-        font-size: 5rem;
+        font-weight: 900;
+        font-size: 5vw;
+    }
+    a {
+        text-align: center;
+        font-family: 'Rubik', sans-serif;
+    }
+    button {
+        font-family: 'Rubik', sans-serif;
     }
 </style>
